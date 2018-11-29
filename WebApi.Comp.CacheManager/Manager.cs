52bc3bb2866f6a8a37083c8095ad5313907b2457
yueqichen <c@yueee.xyz>
2018-11-29 17:57:17 +0800

    添加缓存引用

---
+++ WebApi.Comp.CacheManager/Manager.cs
@@ -0,0 +1,21 @@
+﻿using CacheManager.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Comp.CacheManager
+{
+   public class Manager
+    {
+        ICacheManager<object> RuntimeCache = CacheFactory.Build("getStartedCache", settings =>
+        {
+            settings.WithSystemRuntimeCacheHandle("RuntimeCache");
+        });
+
+        public void text()
+        {
+        }
+    }
+}
